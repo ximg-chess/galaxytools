a5c312f1e27d1bfa2fbb999b5c6f107be4172bf8
Rolf Verberg <rv43@cornell.edu>
2022-04-06 16:32:22 -0400

    test: Minor changes after testing locally and in Galaxy
    Scripts up to date with tomo git tag:
    f2bd41afe72376e5f8921a7fd3dcc32dc5e3e8bb

--- tools/tomo/msnc_tools.py
+++ tools/tomo/msnc_tools.py
@@ -619,9 +619,8 @@ class Config:
         if not os.path.isfile(config_file):
             logging.error(f'Unable to load {config_file}')
             return
-        print(f'\nLoading {config_file}\n')
 
-        # Load config file
+        # Load config file (for now for Galaxy, allow .dat extension)
         self.suffix = os.path.splitext(config_file)[1]
         if self.suffix == '.yml' or self.suffix == '.yaml' or self.suffix == '.dat':
             with open(config_file, 'r') as f:
