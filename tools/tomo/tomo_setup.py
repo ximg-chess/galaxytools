0b92f646a095298322c09fd2731602fa8994f76c
Rolf Verberg <rv43@cornell.edu>
2022-07-28 13:43:47 -0400

    fix: debugging tomo_setup

--- tools/tomo/tomo_setup.py
+++ tools/tomo/tomo_setup.py
@@ -65,8 +65,6 @@ def __main__():
     logging.debug(f'tomoranges = {args.tomo_ranges}')
 
     # Check input file type
-    print(f'inputfiles ({type(args.inputfiles)}):\n{args.inputfiles}')
-    print(f'inputfile_types ({type(args.inputfile_types)}):\n{args.inputfile_types}')
     if isinstance(args.inputfile_types, list):
         if len(args.inputfile_types) == 1 and args.inputfile_types[0] == 'collection':
             if len(args.inputfiles) != 1 or args.inputfiles[0] != 'inputfiles.txt':
@@ -121,9 +119,6 @@ def __main__():
                 for filetype, filepath in zip(args.inputfile_types, args.inputfiles)]
     logging.debug(f'collections:\n{collections}')
     if len(args.tomo_ranges) != 2*len(collections):
-        print(f'datasets ({type(datasets)} {len(datasets)}):\n{datasets}')
-        print(f'collections ({type(collections)} {len(collections)}):\n{collections}')
-        print(f'ranges ({type(args.tomo_ranges)} {len(args.tomo_ranges)}):\n{args.tomo_ranges}')
         raise ValueError('Inconsistent tomo ranges size.')
 
     # Instantiate Tomo object
